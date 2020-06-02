937589d7e82cb6e8cffb8f665023cdf7e83633b6
MAC <miguelangelcabrera1@hotmail.com>
2020-06-02 21:31:46 +0200

    Add help and hora commands

--- mossegaBot.py
+++ mossegaBot.py
@@ -1,8 +1,21 @@
 from telegram.ext import Updater, CommandHandler
+import datetime
 
 # defineix una funció que saluda i que s'executarà quan el bot rebi el missatge /start
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Hola! Soc un bot bàsic per a mossegalapoma!.")
+
+def help(update, context):
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Soc un bot amb comandes /start, /help i /hora.")
+
+
+def hora(update, context):
+    missatge = str(datetime.datetime.now())
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=missatge)
 
 TOKEN = open("token.txt").read().strip()
 
@@ -10,5 +23,7 @@ updater = Updater(token=TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
 dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(CommandHandler('help', help))
+dispatcher.add_handler(CommandHandler('hora', hora))
 
 updater.start_polling()
