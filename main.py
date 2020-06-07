223f80839ee9c2cd9ad4ca5ff4cb6366bfde8c37
MAC <miguelangelcabrera1@hotmail.com>
2020-06-07 10:40:10 +0200

    Renamed file to main and prepared a webhook for gcloud function, passing the user text to lower case for filter PropostesMosseguis or PrOPoStA too

--- main.py
+++ main.py
@@ -1,32 +1,33 @@
+import telegram
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import datetime
 import logging
-from communication import get_updates_json, send_message, reply_message
+import os
 
-
+bot = telegram.Bot(token=os.environ["TELEGRAM_TOKEN"])
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
 
 # defineix una funció que saluda i que s'executarà quan el bot rebi el missatge /start
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Hola! Sóc un bot bàsic per a mossegalapoma!.")
+def start(update, bot):
+    bot.send_message(chat_id=update.effective_chat.id, text="Hola! Sóc un bot bàsic per a mossegalapoma!.")
 
-def help(update, context):
-    context.bot.send_message(
+def help(update, bot):
+    bot.send_message(
         chat_id=update.effective_chat.id,
         text="Sóc un bot amb comandes /start, /help i /hora.")
 
 
-def hora(update, context):
+def hora(update, bot):
     missatge = str(datetime.datetime.now())
-    context.bot.send_message(
+    bot.send_message(
         chat_id=update.effective_chat.id,
         text=missatge)
 
-def unknown(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, reply_to_message_id=update.message.message_id, text="Perdona però no entenc aquesta comanda 🤨")
+def unknown(update, bot):
+    bot.send_message(chat_id=update.effective_chat.id, reply_to_message_id=update.message.message_id, text="Perdona però no entenc aquesta comanda 🤨")
 
-def filter_hashtag_messages(update, context):
+def filter_hashtag_messages(update, bot):
     #user text
     user_text = update.message.text
     telegram_user = update.message.from_user
@@ -45,13 +46,23 @@ def filter_hashtag_messages(update, context):
     #filter #propostamossegui or #proposta or #propostesmossegui or #propostamosseguis text messages
     hashtags = ['#propostamossegui','#proposta','#propostesmossegui','#propostamosseguis', 'proposta', 'propostes']
 
-    if any(hashtag for hashtag in hashtags if hashtag in user_text):
-        context.bot.send_message(chat_id=update.effective_chat.id, reply_to_message_id=update.message.message_id, text="Anoto la proposta!")
-        context.bot.send_message(chat_id=-291751171, text=user_name + "("+ user_first_name + " " + user_last_name + "): " + user_text)
+    if any(hashtag for hashtag in hashtags if hashtag in user_text.lower()):
+        bot.send_message(chat_id=update.effective_chat.id, reply_to_message_id=update.message.message_id, text="Anoto la proposta!")
+        bot.send_message(chat_id=-291751171, text=user_name + "("+ user_first_name + " " + user_last_name + "): " + user_text)
 
-TOKEN = open("token.txt").read().strip()
+#based in https://github.com/python-telegram-bot/python-telegram-bot/wiki/Webhooks
+def webhook(request):
+    bot = telegram.Bot(token=os.environ["TELEGRAM_TOKEN"])
+    if request.method == 'POST':
+        update = telegram.Update.de_json(request.get_json(force=True), bot)
+        filter_hashtag_messages(update, bot)
+    return 'ok'
 
+'''
 updater = Updater(token=TOKEN, use_context=True)
+
+
+
 dispatcher = updater.dispatcher
 
 dispatcher.add_handler(CommandHandler('start', start))
@@ -64,7 +75,4 @@ dispatcher.add_handler(unknown_handler)
 dispatcher.add_handler(MessageHandler(Filters.entity("hashtag"), filter_hashtag_messages))
 
 updater.start_polling()
-
-
-def main():
-    
+''' 
