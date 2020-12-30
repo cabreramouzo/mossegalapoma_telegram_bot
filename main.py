81a4cdf2b0e7a48775ff3ff93a35d7d2dd706c2c
MAC <miguelangelcabrera1@hotmail.com>
2020-12-30 21:47:09 +0100

    Move replies to file

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import requests
 import json
 
 from hashtags import HT_PROPOSTES, HT_FEDERATES, HT_PALASACA, HT_THISISTHEWAY
+from replies import text_troll_reply, text_reply_proposal, text_reply_palasaca, text_rich_reply_proposal
 
 
 INTERNAL_VERSION = '1.0.5'
@@ -135,36 +136,6 @@ def filter_hashtag_messages(update, bot):
         fire = u'\U0001F525'
         water_faucet = u'\U0001F6B0'
         money_bag = u'\U0001F4B0'
-
-        text_troll_reply = [
-            f"Això no té pinta de proposta... {unamused_face}", f"Ets un troll!!! {tongue_out}",
-            f"Ho tens clar! {expression_less}", f"Alerta...TROLL!!! {siren}{siren}{siren}"
-        ]
-
-        text_reply_proposal = [
-            f"Anoto la proposta! {biceps}",f"Proposta anotada {winking_face}", f"Els hi anoto la proposta {grinning_face_smiling_eyes}", 
-            f"L'apunto! {closed_mailbox}", f"Els la deixo al guió {postbox}", f"Apuntada! {paperclip}", f"Proposta que no s'escapa! {robot}{right_arrow}{writing_hand}",
-            f"Viatjant cap al guió... {rocket}", f"Clar que sí! {robot} {robot_arm}", f"Vols dir que no la vas dir fa un temps? {thinking_face}", 
-            f"Aquesta sí que és bona! {clap_hands}{clap_hands}{clap_hands}", f"Vamos allá, ¿No? {sun_glasses}", f"Fot-li! {face_tongue}"
-        ]
-
-        text_reply_palasaca = [
-            f"En @tomasmanz i tot l'equip de Mossegalapoma t'estem molt agraïts! {face_blowing_a_kiss}",f"Disfruta-ho i moltes gràcies! {grinning_face_smiling_eyes} ",
-            f"Gràcies! Cada dia estem més aprop del Tesla Model S... {face_tongue}", f"La targeta fot fum!!! {fire}{gust_of_wind}",
-            f"De mica en mica s'omple la pica! {water_faucet}{money_bag}"
-        ]
-
-        text_rich_reply_proposal = [
-            f"Tota la raó, és del milloret que he vist últimament a Netflix.",
-            f"A Netflix oi? La vaig veure ahir vespre! {winking_face}",
-            f"Ah, pensava que era d'HBO aquesta...{thinking_face}",
-            f"Netflix té merda per un tub, però també de bones i aquesta n'és una {face_tongue}",
-            f"Llàstima que em vaig passar a Disney+ fa temps {sun_glasses}",
-            f"Vaja! Ara que he cancel·lat la subscripció la dius... {expression_less}",
-            f"Enganxa eh!? {face_tongue}",
-            f"Va, que no tot és Netflix en aquesta vida! {grinning_face_smiling_eyes}",
-            f"Podeu deixar de recomanar pelis i sèries a Netflix?! No hi ha qui pugui compilar! {weary_face}",
-        ]
 
         if user_name != "":
             text_reply_proposal.append(f"Saps @{user_name}, jo també ho anava a proposar... {grinning_face_smiling_eyes}")
