e4f0f4ee7f7f2a95f94f423abcf194e1648b6702
MAC <miguelangelcabrera1@hotmail.com>
2020-12-30 21:33:29 +0100

    Typo on filename

---
+++ hashtags.py
@@ -0,0 +1,20 @@
+
+
+
+#filter #propostamossegui or #proposta or #propostesmossegui or #propostamosseguis text messages
+
+"""definition of hastahgs"""
+HT_PROPOSTES = [
+    '#propostamossegui','#proposta','#propostesmossegui', '#propostesmosseguis',
+    '#propostamosseguis', '#propostamosegui', '#propostamoseguis', '#propostesmosegui',
+    '#propostesmoseguis'
+]
+HT_FEDERATES = [
+    '#federrates','#federrades',
+    '#federates', "fe d'errates"
+]
+HT_PALASACA = [
+    '#palasaca','#amazon', '#palasaka', '#afiliats', 'compra per afiliats', 'compra feta per afiliats'
+]
+
+HT_THISISTHEWAY = ['#thisistheway', '#thiswastheway', '#aquesteselcami', '#this_is_the_way', '#aquest_es_el_cami', '#mandalorian', '#themandalorian']
